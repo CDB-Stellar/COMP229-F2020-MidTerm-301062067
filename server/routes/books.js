1a4cb567f05fb7153836e77810d1ec9beadfd61b
Chloe <chloe_baker@live.com>
2020-10-30 16:07:27 -0400

    Finished
    - created a MongoDB Atlas database called fav_books
    - changed the URI 
    - modified the routes/books.js, books/details.ejs, and books/index.ejs files to add in the Add, Edit, and Delete button functionality

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,15 +1,17 @@
+/* Edited by Chloe Baker - 301062067 - October 30, 2020 */
+
 // modules required for routing
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
 // define the book model
-let book = require('../models/books');
+let Book = require('../models/books'); //capitalized Book
 
 /* GET books List page. READ */
 router.get('/', (req, res, next) => {
   // find all books in the books collection
-  book.find( (err, books) => {
+  Book.find( (err, books) => {
     if (err) {
       return console.error(err);
     }
@@ -20,51 +22,112 @@ router.get('/', (req, res, next) => {
       });
     }
   });
-
 });
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+    /****************
+     * MY CODE HERE *
+     ****************/
+    res.render('books/details', { title: 'Add Book', book: "" });
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
+    /****************
+     * MY CODE HERE *
+     ****************/
+    let newBook = Book({ //Book model
+      "Title": req.body.title,
+      "Price": req.body.price,
+      "Author": req.body.author,
+      "Genre": req.body.genre
+    });
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+    Book.create(newBook, (err, Book) => {
+        if (err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the book list
+            res.redirect('/books'); //idk
+        }
+    });
 });
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/:id', (req, res, next) => {
+router.get('/details/:id', (req, res, next) => {
+    /****************
+     * MY CODE HERE *
+     ****************/
+    let id = req.params.id; //search for this id in the database and update it
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    Book.findById(id, (err, bookToEdit) => {
+        if (err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //show the edit view
+            res.render('books/details', {title: 'Edit Book', book: bookToEdit });
+        }
+    });
 });
 
 // POST - process the information passed from the details form and update the document
-router.post('/:id', (req, res, next) => {
+router.post('/details/:id', (req, res, next) => {
+    /****************
+     * MY CODE HERE *
+     ****************/
+    let id = req.params.id;
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    let updatedBook = Book({
+        "_id": id, //going to overwrite the id
+        "Title": req.body.title,
+        "Price": req.body.price,
+        "Author": req.body.author,
+        "Genre": req.body.genre
+    })
 
+    //update the model
+    Book.updateOne({_id: id}, updatedBook, (err) => {
+        if (err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the book list
+            res.redirect('/books');
+        }
+    })
 });
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
+    /****************
+     * MY CODE HERE *
+     ****************/
+    let id = req.params.id;
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    Book.remove({_id: id}, (err) => {
+        if (err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the book list
+            res.redirect('/books');
+        }
+    });
 });
-
 
 module.exports = router;
